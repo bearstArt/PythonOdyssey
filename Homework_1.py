39b057bb25762ec6d898a67f6ede68c09c9b31bf
04artur01 <04artur1993@gmail.com>
2024-05-28 21:55:23 +0400

    Homework_1

---
+++ Homework_1.py
@@ -0,0 +1,8 @@
+print("Hello, world!")
+print("Artur Gharibyan")
+
+num = int(input("Please enter the number\n"))
+if (num % 2) == 0:
+    print("The number is even!")
+else:
+    print("The number is odd!")
