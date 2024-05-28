054d9818a7c2736ab2aca64d83136960179bfbd5
04artur01 <04artur1993@gmail.com>
2024-05-29 01:24:33 +0400

    Homework_1

--- Homework_1.py
+++ Homework_1.py
@@ -1,8 +1,12 @@
+# Գրել ծրագիր, որն էկրանին կտպի “Hello, world!“:
 print("Hello, world!")
+# Գրել ծրագիր, որն էկրանին կտպի ձեր անուն և ազգանունը։
 print("Artur Gharibyan")
 
+# Գրել ծրագիր, որը որպես մուտքային արժեք կստանա թիվ և էկրանին կտպի հաղորդագրություն՝ այն զույգ, թե կենտ թիվ է:
 num = int(input("Please enter the number\n"))
 if (num % 2) == 0:
     print("The number is even!")
 else:
     print("The number is odd!")
+
