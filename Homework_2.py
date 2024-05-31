6b7f5314666c6e4180a9d1513c8bb9462c2a7545
04artur01 <04artur1993@gmail.com>
2024-05-31 13:08:36 +0400

    Update Homework_2.py

--- Homework_2.py
+++ Homework_2.py
@@ -17,8 +17,42 @@ def convert_pounds_to_kg(pounds):
 weight = int(input("Please enter the weight: "))
 
 if choice == 1:
-    print(f'{weight} kg = {convert_kg_to_pounds(weight)} pounds')
+    print(f"{weight} kg = {convert_kg_to_pounds(weight)} pounds")
 elif choice == 2:
-    print(f'{weight} pounds = {convert_pounds_to_kg(weight)} kg')
+    print(f"{weight} pounds = {convert_pounds_to_kg(weight)} kg")
 else:
     print("Error! Wrong input!")
+
+
+# Write a program that asks the user to enter two numbers, x, and y, and computes x − y  /  x+y.
+def summ(x, y):
+    return x + y
+
+
+def div(x, y):
+    return x / y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mat_func1(x, y):
+    if summ(x, y) == 0:
+        print("Error! Can't divide by 0!")
+    else:
+        return div(sub(x, y), summ(x, y))
+
+
+def mat_func2(x, y):
+    if x == 0:
+        print("Error! Can't divide by 0!")
+    else:
+        return summ(sub(x, div(y, x)), y)
+
+
+num1 = int(input("\nPlease enter the number 'x': "))
+num2 = int(input("Please enter the number 'y': "))
+
+print(f"\nVariant 1: (x − y) / (x + y) = {mat_func1(num1, num2)}")
+print(f"Variant 2: x − (y / x) + y = {mat_func2(num1, num2)}")
