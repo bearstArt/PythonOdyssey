87e5e3a8ccca0b8ca44f0b616f1e2383a134f3c9
04artur01 <04artur1993@gmail.com>
2024-05-31 12:34:49 +0400

    Homework_1

--- Homework_2.py
+++ Homework_2.py
@@ -1,3 +1,5 @@
+# Write a program that asks the user for weight in kilograms and converts it to pounds.
+coefficient = 0.45359237
 print("\nEnter parameters for conversion!!")
 choice = int(input("1. Convert kg to pounds\n"
                    "2. Convert pounds to kg"
@@ -5,18 +7,18 @@ choice = int(input("1. Convert kg to pounds\n"
 
 
 def convert_kg_to_pounds(kg):
-    return kg * 2.20462
+    return kg / coefficient
 
 
 def convert_pounds_to_kg(pounds):
-    return pounds * 0.453592
+    return pounds * coefficient
 
 
 weight = int(input("Please enter the weight: "))
 
 if choice == 1:
-    print(f'{convert_kg_to_pounds(weight)} kg')
+    print(f'{weight} kg = {convert_kg_to_pounds(weight)} pounds')
 elif choice == 2:
-    print(f'{convert_pounds_to_kg(weight)} pounds')
+    print(f'{weight} pounds = {convert_pounds_to_kg(weight)} kg')
 else:
     print("Error! Wrong input!")
